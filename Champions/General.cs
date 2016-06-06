ec8f8af39c79815be78cf289a96be3953c3054b1
Benji <Benji@altmansoftwaredesign.com>
2016-06-06 01:27:44 -0500

    stuff done

---
+++ Champions/General.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standard
+{
+	public static class General
+	{
+		public static bool isDigitOnly(string input)
+		{
+			return input.All(char.IsDigit);
+		}
+
+		public static bool isDigitOnly(char input)
+		{
+			return char.IsDigit(input);
+		}
+	}
+}
